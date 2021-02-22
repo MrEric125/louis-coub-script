16ff965f86404f4b8b344ffe06776948fe90df7d
jun.liu <627460581@qq.com>
2021-02-22 20:54:37 +0800

    一个随机生成列表的案例

--- louis_coub_flutter/lib/RandomWords.dart
+++ louis_coub_flutter/lib/RandomWords.dart
@@ -8,10 +8,89 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions=<WordPair>[];
+  final _biggerFont=TextStyle(fontSize: 18.0);
+  final _saved=new Set<WordPair>();
+
+  Widget _buildSuggestions() {
+    return ListView.builder(itemBuilder: (context,i){
+      if(i.isOdd) return Divider();
+      //c除以2 向下取整
+      final index=i ~/2;
+      if(index>=_suggestions.length){
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[index]);
+    });
+  }
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved=_saved.contains(pair);
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved?Icons.favorite:Icons.favorite_border,
+        color: alreadySaved?Colors.red:null,
+
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved) {
+            _saved.remove(pair);
+          }else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+  // 新增代码开始
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+
+          final  tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final  divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("saved suggestion"),
+
+            ),
+            body: ListView(children: divided,),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    
-    final wordPair=WordPair.random();
-    return Text(wordPair.asPascalCase);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
---
+++ louis_coub_flutter/lib/TutorialHome.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class TutorialHome extends StatefulWidget {
+  @override
+  _TutorialHomeState createState() => _TutorialHomeState();
+}
+
+class _TutorialHomeState extends State<TutorialHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
