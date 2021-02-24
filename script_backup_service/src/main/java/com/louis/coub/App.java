f9e55549085b2e7d02d4d26a39b6dc4110d24b53
jun.liu <627460581@qq.com>
2021-02-24 18:04:08 +0800

    小程序

---
+++ script_backup_service/src/main/java/com/louis/coub/App.java
@@ -0,0 +1,17 @@
+package com.louis.coub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Louis
+ * @date created on 2021/2/24
+ * description:
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
