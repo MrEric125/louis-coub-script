13da5e047496237e6a4c72808f3195620aaa4939
jun.liu <627460581@qq.com>
2021-02-25 18:46:56 +0800

    路由跳转

--- louis_coub_wechat/pages/logs/logs.js
+++ louis_coub_wechat/pages/logs/logs.js
@@ -1,4 +1,4 @@
-// pages/index1.js
+// pages/logs/logs.js
 Page({
 
   /**
@@ -12,17 +12,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log("option")
-    
-
-  },
-  handleParent(){
-    console.log("parent")
-  },
-  toLogs(){
-    wx.navigateTo({
-      url: '/pages/logs/logs',
-    })
 
   },
 
